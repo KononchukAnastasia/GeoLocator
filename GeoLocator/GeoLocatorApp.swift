6998ef0e8c03ac96d8db09425c88768961899863
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-06-30 23:07:05 +0700

    Initial Commit

---
+++ GeoLocator/GeoLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoLocatorApp.swift
+//  GeoLocator
+//
+//  Created by Анастасия Конончук on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeoLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
